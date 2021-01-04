4b92743730d98ddcd3fa12051ab3c20569acb33a
= <=>
2021-01-04 16:22:34 +0400

    fixed bugs

--- openaiapi/settings.py
+++ openaiapi/settings.py
@@ -16,7 +16,7 @@ import datetime
 # django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+HTTP_HOST = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
