dfcfcdc2d00e484f6fac59aa9abeedc1a386823b
= <=>
2021-01-04 16:45:14 +0400

    fixed bugs

--- openaiapi/urls.py
+++ openaiapi/urls.py
@@ -28,8 +28,6 @@ schema_view = get_schema_view(
         title="openai api",
         default_version='v1',
         description="Test description",
-        terms_of_service="https://www.ourapp.com/policies/terms/",
-        contact=openapi.Contact(email="contact@expenses.local"),
         license=openapi.License(name="Test License"),
     ),
     public=True,
